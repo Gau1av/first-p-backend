55ad427a8e25e417c0bc7485809a46b5d7c6c314
Gaurav kumar <rgunjan8651@gmail.com>
2026-05-12 19:38:37 +0530

    Fix port for deployment

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const express =require('express');
 const app=express();
+const PORT=process.env.PORT || 5000;
 app.get('/',(req,res)=>res.send("server running"));
+app.listen(PORT,()=>console.log(`server running on port ${PORT}`));
